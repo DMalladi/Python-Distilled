e81e912141a304bbb4ed53c87815bff331329f5b
Deeraj Malladi <Deeraj.Malladi@outlook.com>
2022-01-04 20:22:43 -0600

    Expressions

---
+++ Operators_Expressions_DataManipulations/expressions.py
@@ -0,0 +1,14 @@
+# An expression represents a computation that evaluates to a value.
+# It consists of a combination of literals, names, operators, function or 
+# method calls, e.g. value = 2 + 3 * 5 + sqrt(5 + 7)
+# examples of Operators are Addition (+), multiplication (*)
+# left hand side of assignment represents a location where a reference to an 
+# object is stored in memory
+value = 3 
+
+# Assignment Expression Operator (:=) used to perform combined action of 
+# expression evaluation & assignment with statements such as "if" and "while"
+if name := True:
+    print(name)
+
+
